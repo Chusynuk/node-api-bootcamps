b9abd990f80cad6013b26316bf33e0afa641be69
eloi <recycled_loops@hotmail.com>
2020-06-11 12:52:06 +0200

    add error catching && controllers && middleware && models

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({path: './config/config.env'})
 
+// Connect to database
+connectDB();
+
+//Routes files for
+const router = require('./routes/bootcamps');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Mounter routers
+app.use('/api/v1/bootcamps', router)
+
+app.use(errorHandler);
 
 const PORT  = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.brightMagenta.bold))
+
+// handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red );
+    //Close server & and exit process
+    server.close(() => process.exit(1));
+})
